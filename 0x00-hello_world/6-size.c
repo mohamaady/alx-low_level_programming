dc903c0375a223ee30c59caf96e61e9dc5b7fdac
mohamed <mohamed.reda.elmohamady@gmail.com>
2023-08-10 18:19:13 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,23 @@
 
 /**
  * main - This is the main function
- * Return: 0 indicating successful execution
- */
+ * Return: 0 
+ * /
+#include <stdio.h>
+
 int main(void)
 {
-int inttype;
-float floattype;
-double doubletype;
-char chartype;
-printf("Size of int: %zu bytes\n", sizeof(inttype));
-printf("Size of float: %zu bytes\n", sizeof(floattype));
-printf("Size of double: %zu bytes\n", sizeof(doubletype));
-printf("Size of char: %zu byte\n", sizeof(chartype));
+    printf("Size of a char: %zu byte(s)\n", sizeof(char));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int));
+
+    #if __SIZEOF_LONG__ == 4
+        printf("Size of a long int: 4 byte(s)\n");
+    #elif __SIZEOF_LONG__ == 8
+        printf("Size of a long int: 8 byte(s)\n");
+    #else
+        printf("Size of a long int: Unknown\n");
+#endif
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
