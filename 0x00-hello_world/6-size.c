fe677ec35df1047e2ce89af814c132d526ae50fd
mohamed <mohamed.reda.elmohamady@gmail.com>
2023-08-10 18:05:07 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,14 @@
  */
 int main()
 {
-    int inttype;
-    float floattype;
-    double doubletype;
-    char chartype;
-    printf("Size of int: %zu bytes\n", sizeof(inttype));
-    printf("Size of float: %zu bytes\n", sizeof(floattype));
-    printf("Size of double: %zu bytes\n", sizeof(doubletype));
-    printf("Size of char: %zu byte\n", sizeof(chartype));
-
-    return 0;
+int inttype;
+float floattype;
+double doubletype;
+char chartype;
+printf("Size of int: %zu bytes\n", sizeof(inttype));
+printf("Size of float: %zu bytes\n", sizeof(floattype));
+printf("Size of double: %zu bytes\n", sizeof(doubletype));
+printf("Size of char: %zu byte\n", sizeof(chartype));
+return (0);
 }
 
