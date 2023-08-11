782ddc78ed746e400af9915f32b170b663493865
mohamed <mohamed.reda.elmohamady@gmail.com>
2023-08-10 17:29:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include<stdio.h>
 int main(void)
-{
-/**
- * main -this  is the main function
- * return alwayes 0
- * */
-	
+{	
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
-
 }
