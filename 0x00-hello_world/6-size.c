d8f7822e354aab6afeacef97b903b9747727cba6
mohamed <mohamed.reda.elmohamady@gmail.com>
2023-08-10 18:24:04 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,4 @@ printf("Size of float: %zu bytes\n", sizeof(floattype));
 printf("Size of double: %zu bytes\n", sizeof(doubletype));
 printf("Size of char: %zu byte\n", sizeof(chartype));
 return (0);
-}
-~     
+}  
