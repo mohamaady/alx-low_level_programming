ecc5bb73468bd0fcfbd8b67d94f164905725aa6c
mohamed <mohamed.reda.elmohamady@gmail.com>
2023-08-10 18:14:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,15 +4,15 @@
  * main - This is the main function
  * Return: 0 indicating successful execution
  */
-int main(void) 
+int main(void)
 {
-int inttype; 
+int inttype;
 float floattype;
 double doubletype;
 char chartype;
 printf("Size of int: %zu bytes\n", sizeof(inttype));
 printf("Size of float: %zu bytes\n", sizeof(floattype));
 printf("Size of double: %zu bytes\n", sizeof(doubletype));
-printf("Size of char: %zu byte\n", sizeof(chartype));    
+printf("Size of char: %zu byte\n", sizeof(chartype));
 return (0);
 }
